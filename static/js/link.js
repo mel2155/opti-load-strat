7961e7d823257e74af76937e7cd6674c0337235b
mel2155 <melyy179@gmail.com>
2025-06-30 20:37:34 +0800

    Update link.js

--- static/js/link.js
+++ static/js/link.js
@@ -1,9 +1,9 @@
 var link_mb = {
-                kyH5:'https://www.ie6vxj.vip:8443/entry/register87685/?i_code=36153549',//  kyh5       ky
-                kyApp:'https://www.z7nuv1.vip:7443/87685?i_code=36153549',//                kyapp
+                kyH5:'https://www.x63lus.com:9133/register54695/?i_code=36153549',//  kyh5       ky
+                kyApp:'https://www.x63lus.com:9133/register54695/?i_code=36153549',//            kyapp
                 
-                lyH5:'https://www.s6q7m.com:9972/register25371/?i_code=2298291',//  lyh5.     ly
-                lyApp:'https://www.s6q7m.com:9972/register25371/?i_code=2298291',//.               lyxapp
+                lyH5:'https://www.s6q7m.com:9133/register18218/?i_code=2298291',//  lyh5.     ly
+                lyApp:'https://www.s6q7m.com:9133/register18218/?i_code=2298291',//.               lyxapp
                 
                 jyH5:'https://www.s6q7m.com:9972/register25371/?i_code=2298291',//       jyh5.      9you
                 jyApp:'https://www.s6q7m.com:9972/register25371/?i_code=2298291',//                  jyapp
@@ -16,7 +16,7 @@ var link_mb = {
                 
 var link_pc = {
                 kyH5:'https://www.x63lus.com:9133/register87685/?i_code=36153549',//.        kypc       ky
-                kyApp:'https://www.z7nuv1.vip:7443/87685?i_code=36153549',//        kypc
+                kyApp:'https://www.x63lus.com:9133/register54695/?i_code=36153549',//        kypc
                 
                 lyH5:'https://www.s6q7m.com:9972/register78499/?i_code=2298291',//         lypc      ly
                 lyApp:'https://www.s6q7m.com:9972/register25371/?i_code=2298291',//        lypc
@@ -26,7 +26,7 @@ var link_pc = {
                 
                 hthH5:'https://www.s6q7m.com:9972/register78499/?i_code=2298291',//   
                 hthApp:'https://www.s6q7m.com:9972/register25371/?i_code=2298291',// 
-				        ayxH5:'https://www.s6q7m.com:9972/register78499/?i_code=2298291',//   
+		ayxH5:'https://www.s6q7m.com:9972/register78499/?i_code=2298291',//   
                 ayxApp:'https://www.s6q7m.com:9972/register25371/?i_code=2298291',// 
                 }
 function _Reg(key) {
